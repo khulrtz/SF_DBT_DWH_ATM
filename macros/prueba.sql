366efa6aae9374e365200777d1bf9db2fbfd5d08
jvaldiviaparra@gmail.com <jvaldiviaparra@gmail.com>
2022-03-29 09:06:05 +0000

    a

--- macros/prueba.sql
+++ macros/prueba.sql
@@ -1,69 +1,114 @@
 
 {%- macro pivotar(table_name='T_C1_SAT_CUSTOMER') -%}
 
+{%- set config_query -%}
+    select 
+    MAX(DECODE(DB_ORIGEN,'CALCULADO',NULL,db_origen)) db_origen,
+    MAX(DECODE(DB_ORIGEN,'CALCULADO',NULL,tb_origen)) tb_origen,
+    MAX(db_destino)DB_DESTINO,
+    MAX(tabla_destino)TB_DESTINO,
+    MAX(decode(in_ts_scd,'S',CL_DESTINO,NULL)) COL_TS_SCD,
+    MAX(decode(in_key,'S',CL_DESTINO,NULL)) COL_KEY_SCD
+    from SF_DBT_DWH_ATM.DBT_DEV.mapeos
+    where TABLA_DESTINO = '{{ table_name }}'
+{%- endset -%}
+
+{%- set config_results = run_query(config_query) -%}
+
+{%- if execute -%}
+    {%- set db_origen = config_results.columns[0].values() -%}
+    {%- set tb_origen = config_results.columns[1].values() -%}
+    {%- set db_destino = config_results.columns[2].values() -%}
+    {%- set tb_destino = config_results.columns[3].values() -%}
+    {%- set col_ts_scd = config_results.columns[4].values() -%}
+    {%- set col_key_scd = config_results.columns[5].values() -%}
+{%- else -%}
+    {%- set db_origen = [] -%}
+    {%- set tb_origen = [] -%}
+    {%- set db_destino = [] -%}
+    {%- set tb_destino = [] -%}
+    {%- set col_ts_scd = [] -%}   
+    {%- set col_key_scd = [] -%}   
+{%- endif -%}
+
+{%- set db_origen = db_origen[0] -%}
+{%- set tb_origen = tb_origen[0] -%}
+{%- set db_destino = db_destino[0] -%}
+{%- set tb_destino = tb_destino[0] -%}
+{%- set col_ts_scd = col_ts_scd[0] -%}
+{%- set col_key_scd = col_key_scd[0] -%}
+
+    {{
+        config(
+            unique_key=col_key_scd,
+            strategy='timestamp',
+            updated_at=col_ts_scd
+        )
+    }}
+
+   
+
+
+
 {%- set values_query -%}
-select distinct
-db_origen,
-tb_origen,
-cl_origen,
-db_destino,
-tabla_destino,
-cl_destino,
-in_hash,
-in_trim,
-in_ts_scd,
-in_key
-from {{ ref('mapeos') }}
-where TABLA_DESTINO = '{{ table_name }}'
+    select distinct
+    tb_origen,
+    cl_origen,
+    cl_destino,
+    in_hash,
+    in_trim
+    from {{ ref('mapeos') }}
+    where TABLA_DESTINO = '{{ table_name }}'
 {%- endset -%}
 
 {%- set results = run_query(values_query) -%}
 
-{%- for i in results -%}
+SELECT 
+{% for i in results -%}
     {%- if execute -%}
-        {%- set db_origen = results.columns[0].values() -%}
-        {%- set tb_origen = results.columns[1].values() -%}
-        {%- set cl_origen = results.columns[2].values() -%}
-        {%- set db_destino = results.columns[3].values() -%}
-        {%- set tabla_destino = results.columns[4].values() -%}
-        {%- set cl_destino = results.columns[5].values() -%}
-        {%- set in_hash = results.columns[6].values() -%}
-        {%- set in_trim = results.columns[7].values() -%}
-        {%- set in_ts_scd = results.columns[8].values() -%}
-        {%- set in_key = results.columns[9].values() -%}
+        {%- set tb_origen = results.columns[0].values() -%}
+        {%- set cl_origen = results.columns[1].values() -%}
+        {%- set cl_destino = results.columns[2].values() -%}
+        {%- set in_hash = results.columns[3].values() -%}
+        {%- set in_trim = results.columns[4].values() -%}
         {%- else -%}
-        {%- set db_origen = [] -%}
         {%- set tb_origen = [] -%}
         {%- set cl_origen = [] -%}
-        {%- set db_destino = [] -%}
-        {%- set tabla_destino = [] -%}
         {%- set cl_destino = [] -%}
         {%- set in_hash = [] -%}
-        {%- set in_trim = [] -%}
-        {%- set in_ts_scd = [] -%}
-        {%- set in_key = [] -%}        
+        {%- set in_trim = [] -%}   
     {%- endif -%}
-        {%- set db_origen = db_origen[loop.index-1] -%}
         {%- set tb_origen = tb_origen[loop.index-1] -%}
         {%- set cl_origen = cl_origen[loop.index-1] -%}
-        {%- set db_destino = db_destino[loop.index-1] -%}
-        {%- set tabla_destino =tabla_destino[loop.index-1] -%}
         {%- set cl_destino = cl_destino[loop.index-1] -%}
         {%- set in_hash = in_hash[loop.index-1] -%}
         {%- set in_trim = in_trim[loop.index-1] -%}
-        {%- set in_ts_scd = in_ts_scd[loop.index-1] -%}
-        {%- set in_key = in_key[loop.index-1] -%}
-    
-    {%- if in_hash == 'S' -%}
-        {%- raw -%}{{{%- endraw -%}dbt_utils.surrogate_key({{- cl_origen -}}){%- raw -%}}}{%- endraw %} AS {{ cl_destino -}}
-    {% else %}
-    {{ cl_origen}} AS {{ cl_destino }}
+
+{# INICIALIZACION #}   
+    {%- set campo = cl_origen -%} 
+{# IN_TRIM #}
+    {%- if in_trim == 'S' -%}
+        {%- set campo = literal('RTRIM(LTRIM(') ~ campo ~ literal('))') -%} 
     {%- endif -%}
-   
-   {{ campo }}
-
-{%- endfor -%}
-
-
-
+{# IN_HASH #}
+    {%- if in_hash == 'S' -%}
+        {%- set campo = dbt_utils.surrogate_key( cl_origen ) -%} 
+    {%- endif -%}
+ {# CAMPOS_CALCULADOS #}     
+    {%- if tb_origen == 'CALCULADO' -%}
+        {% if cl_origen == 'ORIGEN' %}
+            {%- set campo = db_origen  ~ literal('.') ~ tb_origen -%}
+        {%- elif cl_origen == 'LOADED' -%}
+             {%- set campo = dbt_utils.current_timestamp() -%}
+        {%- endif -%} 
+    {%- endif -%}
+{# ALIAS #}
+    {%- set campo = campo  ~ literal(' AS ') ~ cl_destino -%}
+{# COMA #}
+    {%- if not loop.last -%}
+        {%- set campo = campo  ~ literal(',') -%}   
+    {%- endif -%}
+    {{ campo }}
+{% endfor -%}
+FROM {{ ref('mapeos') }}
 {%- endmacro -%}
